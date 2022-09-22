72d9d81be4d98df3891f3088a5d569cfe6cf2832
renren121314 <renren199612@gmail.com>
2022-09-22 10:05:27 +0000

    chapter3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
-  get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
+  Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lists/new'
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+end
 end
