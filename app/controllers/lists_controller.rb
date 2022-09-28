bcf5ece2476d4bacfed82cbc797ba5483cba6b7d
renren121314 <renren199612@gmail.com>
2022-09-28 01:33:21 +0000

    yersterday

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,6 +14,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ Rails.application.routes.draw do
   get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
-  Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'lists/new'
-  get 'top' => 'homes#top'
-  post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-end
+  get 'lists/:id' => 'lists#show'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'top' => 'homes#top'
 end
