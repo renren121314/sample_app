e296f55ef9cb46ddb79ae181f3a74f052fdd969b
renren121314 <renren199612@gmail.com>
2022-10-03 07:28:53 +0000

    chapter11

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists/show'
   get '/top' => 'homes#top'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show',as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'top' => 'homes#top'
 end
